87ce675079ba7699348f7bf5cc0b5789a2ffb23d
imanj1470 <imanj1470@gmail.com>
2026-05-20 02:06:48 +0100

    Add: combination-target-sum - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        finalResults = []
+        def checkPath(options, resultArr, currentTarget):
+            if currentTarget == 0:
+                return
+            
+            for i, option in enumerate(options):
+                workingSum = option
+
+                if workingSum == currentTarget:
+                    finalResults.append(resultArr + [option])
+                    return
+
+                if workingSum > currentTarget:
+                    return
+                elif workingSum < currentTarget:
+                    newTarget = currentTarget - workingSum
+
+                    res = checkPath(options[:i+1], resultArr + [option], newTarget)
+            return resultArr
+
+        checkPath(nums, [], target)
+        return finalResults
