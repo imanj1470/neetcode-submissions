53bb74f9eecae118c655fa978a419eb9c99e8f30
imanj1470 <imanj1470@gmail.com>
2026-05-29 02:32:35 +0100

    Add: two-integer-sum - submission-30

---
+++ Data Structures & Algorithms/two-integer-sum/submission-30.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        unique = set(nums)
+
+        for i, val in enumerate(nums):
+            complement = target - val
+            if complement in unique:
+                print("current looking at i, val: ",i, val)
+                print(f"complement of {complement} found in set")
+                potentialDuplicate = val * 2 == target
+                print(potentialDuplicate)
+                for j in range(len(nums) - 1, i-1, -1):
+                    if nums[j] == complement:
+                        print("found", complement, "at index", j)
+                        if potentialDuplicate and i == j: # 3+3 = 6 but so does 2+4. should continue
+                            break
+                        else:
+                            return [i,j] 
+        return []
